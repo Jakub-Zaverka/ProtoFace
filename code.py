5ba8c0af6c846d9fe4a93aab3ebda73d86cb6bfa
Jakub-Zaverka <zaverka.jakub@gmail.com>
2026-04-12 15:12:05 +0200

    emotes

--- code.py
+++ code.py
@@ -4,6 +4,8 @@ from accelerometer import Accelerometer
 from mic import Microphone
 from I2C_sim import APDSSensor
 from I2C_sim import OLEDDisplay
+import board
+import digitalio
 
 
 
@@ -61,8 +63,50 @@ if SSD1306_ON:
 
 
 BLINK_TIME_SET = 10
+BOOP_TIMER = 5
 blink_time = 0
 eye_closed = False
+boop = False
+boop_count = 0
+
+
+btn_down = digitalio.DigitalInOut(board.BUTTON_DOWN)
+btn_up = digitalio.DigitalInOut(board.BUTTON_UP)
+
+EMOTE_TIMER = 10
+emote_time = 0
+emote = False
+
+
+def handle_emote(
+    button_pressed,
+    emote_active,
+    emote_time,
+    display,
+    target_matrix,
+    active_bmp,
+    idle_bmp,
+    timer_limit,
+    verbose=False,
+):
+    if button_pressed:
+        if not emote_active:
+            display.load_bmp_into_matrix(target_matrix, active_bmp)
+        emote_active = True
+        emote_time = 0
+        if verbose:
+            print("emote")
+            print("pressed down")
+    elif emote_active:
+        emote_time += 1
+
+        if emote_time >= timer_limit:
+            emote_active = False
+            emote_time = 0
+            display.load_bmp_into_matrix(target_matrix, idle_bmp)
+
+    return emote_active, emote_time
+
 
 while True:
     #if movement
@@ -72,44 +116,100 @@ while True:
             #correct accelerometer setting
             eye_matrix["tile"].x -= int(accelerometer.derivation()[0])
             eye_matrix["tile"].y += int(accelerometer.derivation()[1])
+            nose_matrix["tile"].x -= int(accelerometer.derivation()[0])
+            nose_matrix["tile"].y += int(accelerometer.derivation()[1])
+            mouth_matrix["tile"].x -= int(accelerometer.derivation()[0])
+            mouth_matrix["tile"].y += int(accelerometer.derivation()[1])
         else:
             eye_matrix["tile"].x = 31
             eye_matrix["tile"].y = 0
+            nose_matrix["tile"].x = 0
+            nose_matrix["tile"].y = 0
+            mouth_matrix["tile"].x = 0
+            mouth_matrix["tile"].y = 16
             pass
     
     if MIC_ON:
-        if mic.get_value() > 20:
+        if mic.get_value() > 10:
             display.load_bmp_into_matrix(mouth_matrix, "/faces/mouth_speak.bmp")
-            print("speak")
+            if VERBOSE: 
+                print("speak")
         else:
             display.load_bmp_into_matrix(mouth_matrix, "/faces/mouth.bmp")
 
     
 
+    #Blinking
+    if not boop and not emote:
+        if blink_time >= BLINK_TIME_SET and not eye_closed:
+            eye_closed = True
+            display.load_bmp_into_matrix(eye_matrix, "/faces/eye_blink.bmp")
+            blink_time = 0
 
-    if blink_time >= BLINK_TIME_SET and not eye_closed:
-        eye_closed = True
-        display.load_bmp_into_matrix(eye_matrix, "/faces/eye_blink.bmp")
-        blink_time = 0
+        elif blink_time >= BLINK_TIME_SET // 6 and eye_closed:
+            eye_closed = False
+            display.load_bmp_into_matrix(eye_matrix, "/faces/eye.bmp")
+            blink_time = 0
+            if VERBOSE:
+                print("blink")
 
-    elif blink_time >= BLINK_TIME_SET // 6 and eye_closed:
-        eye_closed = False
-        display.load_bmp_into_matrix(eye_matrix, "/faces/eye.bmp")
-        blink_time = 0
-        print("blink")
-
-    else:
-        blink_time += 1
+        else:
+            blink_time += 1
 
 
     if SSD1306_ON:
         oled.show_text("Test")
 
-    if APDS_ON:
-        if apds.get_value() > 200:
-            #boop
-            pass
+    # boop
+    if not emote:
+        if APDS_ON:
+            if apds.get_value() > 200:
+                if not boop:
+                    display.load_bmp_into_matrix(eye_matrix, "/faces/eye_open.bmp")
+                boop = True
+                boop_count = 0
+                eye_closed = False
+                if VERBOSE:
+                    print("boop")
 
+            elif boop:
+                boop_count += 1
+
+                if boop_count >= BOOP_TIMER:
+                    boop = False
+                    boop_count = 0
+                    eye_closed = False
+                    display.load_bmp_into_matrix(eye_matrix, "/faces/eye.bmp")
+
+
+    #Emote
+    emote, emote_time = handle_emote(
+        button_pressed=not btn_down.value,
+        emote_active=emote,
+        emote_time=emote_time,
+        display=display,
+        target_matrix=eye_matrix,
+        active_bmp="/faces/sleep.bmp",
+        idle_bmp="/faces/eye.bmp",
+        timer_limit=EMOTE_TIMER,
+        verbose=VERBOSE,
+    )
+
+    emote, emote_time = handle_emote(
+        button_pressed=not btn_up.value,
+        emote_active=emote,
+        emote_time=emote_time,
+        display=display,
+        target_matrix=eye_matrix,
+        active_bmp="/faces/cross.bmp",
+        idle_bmp="/faces/eye.bmp",
+        timer_limit=EMOTE_TIMER,
+        verbose=VERBOSE,
+    )
+
+
+
+    # debug print
     if VERBOSE:
         if ACCELEROMETER_ON:
             print(f"Accelerometer: {accelerometer.derivation()}")
